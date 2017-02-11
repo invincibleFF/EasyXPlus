bfb3383060dbed11239f41fce06133f3b2f6bcad
lgxZJ <lgxZJ@outlook.com>
2017-02-11 16:53:24 +0800

    -done Geometry module

---
+++ easyXPlus/easyGeometry.cpp
@@ -0,0 +1,354 @@
+#include "easyGeometry.h"
+#include "easyWindow.h"
+#include "easyExcept.h"
+
+#include <cassert>
+
+using namespace easyXPlus;
+
+namespace easyXPlus
+{
+	////////////////////////////////////////////////////////////////////////////////
+	//									PointArray
+
+	PointArray::PointArray(std::initializer_list<Point> points)
+		: points(points)
+	{}
+
+	///////////////////////////////////
+
+	inline bool PointArray::isEmpty() const
+	{
+		return points.empty();
+	}
+
+	///////////////////////////////////
+
+	inline unsigned PointArray::getSize() const
+	{
+		return points.size();
+	}
+
+	///////////////////////////////////
+
+	Point PointArray::getPoint(unsigned index) const
+	{
+		try
+		{
+			return points.at(index);
+		}
+		catch (std::out_of_range exception)
+		{
+			throw EasyExcept("Index error!");
+		}
+	}
+
+	///////////////////////////////////
+
+	void PointArray::appendPoint(Point point)
+	{
+		points.push_back(point);
+	}
+
+	//////////////////////////////////
+
+	void PointArray::insertPoint(unsigned indexBefore, Point point)
+	{
+		if (indexBefore > points.size())
+			throw EasyExcept("Index error");
+
+		points.insert(points.begin() + indexBefore, point);
+	}
+
+	//////////////////////////////////
+
+	void PointArray::assignPoint(unsigned index, Point point)
+	{
+		points.at(index) = point;
+	}
+
+	//////////////////////////////////
+
+	void PointArray::erasePoint(unsigned index)
+	{
+		points.erase(points.begin() + index);
+	}
+
+	//////////////////////////////////
+
+	void PointArray::clearAll()
+	{
+		points.clear();
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	//								Geometry
+
+	//	static variables
+
+	COLORREF Geometry::dotColor = Rgb::White().toColorref();
+	COLORREF Geometry::lineColor = Rgb::White().toColorref();
+	COLORREF Geometry::fillColor = Rgb::White().toColorref();
+
+	std::vector<HPEN>	Geometry::penHandles;
+	std::vector<HBRUSH>	Geometry::brushHandles;
+
+	/////////////////////////////////
+
+	void Geometry::setDotColor(const Colorable& color)
+	{
+		dotColor = color.toColorref();
+	}
+
+	/////////////////////////////////
+
+	void Geometry::setLineColor(const Colorable& color)	{
+		lineColor = color.toColorref();
+
+		HPEN penHandle = CreatePen(PS_SOLID, 0, lineColor);
+		if (NULL == penHandle)
+			throw EasyExcept("System call error!");
+		else
+			penHandles.push_back(penHandle);
+
+		HGDIOBJ objRet = SelectObject(
+			Window::getDefaultDC(), (HGDIOBJ)penHandle);
+
+		if (NULL == objRet)
+			throw EasyExcept("System call error!");
+	}
+
+	/////////////////////////////////////
+
+	void Geometry::setFillColor(const Colorable& color)
+	{
+		fillColor = color.toColorref();
+
+		HBRUSH brushHandle = CreateSolidBrush(fillColor);
+		if (NULL == brushHandle)
+			throw EasyExcept("System call error!");
+		else
+			brushHandles.push_back(brushHandle);
+
+		HGDIOBJ objRet = SelectObject(
+			Window::getDefaultDC(), (HGDIOBJ)brushHandle);
+
+		if (NULL == objRet)
+			throw EasyExcept("System call error!");
+	}
+
+	//////////////////////////////////////
+
+	COLORREF Geometry::getLineColor()
+	{
+		return lineColor;
+	}
+
+	///////////////////////////////////////
+
+	COLORREF Geometry::getFillColor()
+	{
+		return fillColor;
+	}
+
+	///////////////////////////////////////
+
+	COLORREF Geometry::getDotColor()
+	{
+		return dotColor;
+	}
+
+	///////////////////////////////////////
+
+	COLORREF Geometry::getPointColor(Point point)
+	{
+		COLORREF retColor = GetPixel(
+			Window::getDefaultDC(), point.getX(), point.getY());
+
+		if (retColor == CLR_INVALID)
+			throw EasyExcept("System call error!");
+		else
+			return retColor;
+	}
+
+	///////////////////////////////////////
+
+	void Geometry::drawDot(Point point)
+	{
+		COLORREF colorRet = SetPixel(
+			Window::getDefaultDC(), point.getX(), point.getY(), dotColor);
+
+		if (colorRet == -1 || colorRet == ERROR_INVALID_PARAMETER)
+			throw EasyExcept("System call error!");
+	}
+
+	///////////////////////////////////////
+
+	void Geometry::drawLine(Point from, Point to)
+	{
+		HDC hdc = Window::getDefaultDC();
+
+		if (0 == MoveToEx(hdc, from.getX(), from.getY(), NULL))
+			throw EasyExcept("System call error!");
+
+		if (0 == LineTo(hdc, to.getX(), to.getY()))
+			throw EasyExcept("System call error!");
+	}
+
+	///////////////////////////////////////
+
+	void Geometry::drawArc(RectRegion bound, Point start, Point end)
+	{
+		BOOL ret = Arc(
+			Window::getDefaultDC(),
+			bound.getLeftTop().getX(),
+			bound.getLeftTop().getY(),
+			bound.getRightBottom().getX(),
+			bound.getRightBottom().getY(),
+			start.getX(),
+			start.getY(),
+			end.getX(),
+			end.getY());
+
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+	///////////////////////////////////////
+
+	void Geometry::drawBeziers(PointArray points)
+	{
+		//	bezier curves need (3n + 1) point to draw itself
+		if ( (points.getSize() % 3) != 1 )
+			throw EasyExcept("The number of points is invalid!");
+
+		
+		POINT* pointArray = convertToPOINTs(points);
+		BOOL ret = PolyBezier(
+			Window::getDefaultDC(), pointArray, points.getSize());
+
+		delete[] pointArray;
+		if (ret == 0)	throw EasyExcept("System call error");
+	}
+
+	POINT* Geometry::convertToPOINTs(const PointArray& points)
+	{
+		POINT* pointArray = new POINT[points.getSize()];
+		for (unsigned i = 0; i < points.getSize(); ++i)
+			pointArray[i].x = points.getPoint(i).getX(),
+			pointArray[i].y = points.getPoint(i).getY();
+
+		return pointArray;
+	}
+
+	///////////////////////////////////////
+
+	void Geometry::drawPolyline(PointArray points)
+	{
+		//	polyline -> point number greater equal than 2
+		if (points.getSize() < 2)
+			throw EasyExcept("System call error!");
+
+		POINT* pointArray = convertToPOINTs(points);
+		BOOL ret = Polyline(
+			Window::getDefaultDC(), pointArray, points.getSize());
+
+		delete[] pointArray;
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+	/////////////////////////////////////////
+
+	void Geometry::drawChord(RectRegion bound, Point start, Point end)
+	{
+		BOOL ret = Chord(
+			Window::getDefaultDC(),
+			bound.getLeftTop().getX(),
+			bound.getLeftTop().getY(),
+			bound.getRightBottom().getX(),
+			bound.getRightBottom().getY(),
+			start.getX(),
+			start.getY(),
+			end.getX(),
+			end.getY());
+
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+	////////////////////////////////////////
+
+	void Geometry::drawEllipse(RectRegion bound)
+	{
+		BOOL ret = Ellipse(
+			Window::getDefaultDC(),
+			bound.getLeftTop().getX(),
+			bound.getLeftTop().getY(),
+			bound.getRightBottom().getX(),
+			bound.getRightBottom().getY());
+
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+	//////////////////////////////////////////
+
+	void Geometry::drawRectangle(RectRegion rectRegion)
+	{
+		BOOL ret = ::Rectangle(
+			Window::getDefaultDC(),
+			rectRegion.getLeftTop().getX(),
+			rectRegion.getLeftTop().getY(),
+			rectRegion.getRightBottom().getX(),
+			rectRegion.getRightBottom().getY());
+
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+	///////////////////////////////////////////
+
+	void Geometry::drawRoundRectangle(RectRegion rectRegion, Rectangle rectangle)
+	{
+		BOOL ret = RoundRect(
+			Window::getDefaultDC(),
+			rectRegion.getLeftTop().getX(),
+			rectRegion.getLeftTop().getY(),
+			rectRegion.getRightBottom().getX(),
+			rectRegion.getRightBottom().getY(),
+			rectangle.getWidth(),
+			rectangle.getHeight());
+
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+	///////////////////////////////////////////
+
+	void Geometry::drawPie(RectRegion bound, Point start, Point end)
+	{
+		BOOL ret = Pie(
+			Window::getDefaultDC(),
+			bound.getLeftTop().getX(),
+			bound.getLeftTop().getY(),
+			bound.getRightBottom().getY(),
+			bound.getRightBottom().getY(),
+			start.getX(),
+			start.getY(),
+			end.getX(),
+			end.getY());
+
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+	/////////////////////////////////////////////
+
+	void Geometry::drawPolygon(PointArray points)
+	{
+		if (points.getSize() < 2)
+			throw EasyExcept("System call error!");
+
+		POINT* pointArray = convertToPOINTs(points);
+		BOOL ret = Polygon(
+			Window::getDefaultDC(), pointArray, points.getSize());
+
+		delete[] pointArray;
+		if (ret == 0)	throw EasyExcept("System call error!");
+	}
+
+}
---
+++ easyXPlus/easyGeometry.h
@@ -0,0 +1,107 @@
+#ifndef EASY_GEOMETRY_H
+#define EASY_GEOMETRY_H
+
+#include "easyColor.h"
+#include <vector>
+
+namespace easyXPlus
+{
+	//	A point in a coordinate system.
+	class Point
+	{
+	public:
+		Point(unsigned X, unsigned Y) : x(X), y(Y)
+		{}
+
+		unsigned getX() const	{ return x; }
+		unsigned getY() const	{ return y; }
+
+	private:
+		unsigned x, y;
+	};
+
+	//	A Rect-Region is a rectangular area in the coordinate system. It is denoted by
+	//	a left-top point and a right-bottom point.
+	class RectRegion
+	{
+	public:
+		RectRegion(Point leftTopPoint, Point rightBottomPoint)
+			: leftTop(leftTopPoint), rightBottom(rightBottomPoint)
+		{}
+
+		Point getLeftTop() const	{ return leftTop; }
+		Point getRightBottom() const	{ return rightBottom; }
+
+	private:
+		Point leftTop, rightBottom;
+	};
+
+	//	A rectangle is an "RectRegion" without coordinate information. Thus, it is only
+	//	limited by width and height.
+	class Rectangle
+	{
+	public:
+		Rectangle(unsigned width, unsigned height) : width(width), height(height)
+		{}
+
+		unsigned getWidth() const	{ return width; }
+		unsigned getHeight() const	{ return height; }
+
+	private:
+		unsigned width, height;
+	};
+
+	class PointArray
+	{
+	public:
+		PointArray(std::initializer_list<Point> points);
+
+		bool isEmpty() const;
+		unsigned getSize() const;
+		Point getPoint(unsigned index) const;
+		void appendPoint(Point point);
+		void erasePoint(unsigned index);
+		void assignPoint(unsigned index, Point point);
+		void insertPoint(unsigned indexBefore, Point point);
+		void clearAll();
+
+	private:
+		std::vector<Point>	points;
+	};
+
+	class Geometry
+	{
+	public:
+		static COLORREF getDotColor();
+		static COLORREF getLineColor();
+		static COLORREF getFillColor();
+		static void setDotColor(const Colorable& color);
+		static void setLineColor(const Colorable& color);
+		static void setFillColor(const Colorable& color);
+
+		static COLORREF getPointColor(Point point);
+
+		static void drawDot(Point point);
+		static void drawLine(Point from, Point to);
+		static void drawArc(RectRegion bound, Point start, Point end);
+		static void drawBeziers(PointArray points);
+		static void drawPolyline(PointArray points);
+		static void drawChord(RectRegion bound, Point start, Point end);
+		static void drawEllipse(RectRegion bound);
+		static void drawRectangle(RectRegion rectRegion);
+		static void drawRoundRectangle(RectRegion rectRegion, Rectangle ellipseBound);
+		static void drawPie(RectRegion bound, Point start, Point end);
+		static void drawPolygon(PointArray points);
+
+	private:
+		static POINT* convertToPOINTs(const PointArray& points);
+
+		static COLORREF	dotColor, lineColor, fillColor;
+
+		//	handles need to be deleted
+		static std::vector<HPEN>	penHandles;
+		static std::vector<HBRUSH>	brushHandles;
+	};
+}
+
+#endif
